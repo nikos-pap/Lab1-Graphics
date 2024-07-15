7010d354f7c1f2769c13703087023ecfa5f15868
StamatisMat <54558978+StamatisMat@users.noreply.github.com>
2024-07-16 02:32:23 +0300

    Removed Deprecated comments

--- DynamicShapeArray.h
+++ DynamicShapeArray.h
@@ -1,41 +1,11 @@
 #pragma once
 #include "ShapeFactory.h"
-/*
-	Deprecated. Added to ShapeFactory.
-#define PI 3.14159265f
-#define SQRT_2 1.41421356237f
-#define T_CUBE 0
-#define T_SPHERE 1
-#define T_CYLINDER 2
-#define T_RING 3
 
-#define CIRCLE_VERTEX_NUM (CIRCLE_TRIANGLE_NUM+2)
-
-#define SPHERE_SECTOR_NUM 36
-#define SPHERE_STACK_NUM 18
-#define CIRCLE_TRIANGLE_NUM 34
-*/
 #define GLOBAL_SPEED 60
 #define MAX_SPEEDUP 100
 
 
 extern bool soundsEnabled;
-/*
-	Deprecated. Added to ShapeFactory.
-struct Shape {
-	float * data;
-	int size;
-	int shapeType;
-	float speed[3] = { 0.0f ,0.0f ,0.0f };
-	float color[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	unsigned int vao_id = 0;
-	unsigned int vb_id = 0;
-	unsigned int ib_id = 0;
-	glm::mat4 Model;
-	float center[3];
-	float d;
-	float d2;
-};*/
 
 class DynamicShapeArray
 {
@@ -77,31 +47,11 @@ private:
 	void Collide(int index1, int index2);
 	
 	
-	//functions that create shapes
-	//void CreateCube(float x0, float y0, float z0, float size);
-	//void CreateSphere(float x0, float y0, float z0, float radius);
-	//void CreateCylinder(float x, float y, float z, float radius, float height);
-	//void CreateRing(float x0, float y0, float z0, float r1, float r2);
-	//float* CreateCircle(float x, float y, float z, float radius);
-	
-	//handles buffer creation and data
-	//void createBuffer(int index);
-
-	//functions that initialize index arrays they run when shapeArray is initialized
-	//void InitSphereIndices();
-	//void InitCylinderIndices();
-	//void AddCircleIndices(unsigned int* indices, int index, int offset = 0);
 
 	//Extends the Array used when Array size reaches its capacity
 	void Extend();
 
 	//assisting function
 	float * GetNormals(int shapeType);
-	//unsigned int* GetIndexPointer(int index);
-	//void AddShape(float* element, int size, int shapeType, float x0, float y0, float z0, float d);//helps with shape creation
 	void AddShape(Shape shape);
-	
-	//Random functions
-	//int RandomInt(int min, int max);
-	//float RandomFloat(float min, float max);
 };
